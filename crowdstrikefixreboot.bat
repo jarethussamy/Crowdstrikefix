311aa0d89fbbfb3acd1d52deabfdbd1721a61c96
jarethussamy <42392057+jarethussamy@users.noreply.github.com>
2024-07-24 18:25:36 -0400

    Update crowdstrikefixreboot.bat

--- crowdstrikefixreboot.bat
+++ crowdstrikefixreboot.bat
@@ -1,12 +1,13 @@
+
 @echo off
 setlocal
 
 rem Check if running as administrator
 net session >nul 2>&1
-if %errorLevel% neq 0 (
-    echo This script must be run as an administrator.
-    pause
-    exit /b 1
+if %errorlevel% neq 0 (
+  echo This script must be run as an administrator.
+  pause
+  exit /b 1
 )
 
 rem Boot into Safe Mode with Networking
@@ -20,32 +21,31 @@ rem Ensure that we are now in Safe Mode
 :SafeMode
 rem Check if we are in Safe Mode with Networking
 if /i "%SAFEBOOT_OPTION%"=="network" (
-    rem Navigate to the CrowdStrike directory
-    cd /d %WINDIR%\System32\drivers\CrowdStrike
-    
-    rem Delete the matching file
-    set fileDeleted=false
-    for %%f in (C-00000291*.sys) do (
-        if exist "%%f" (
-            del /f /q "%%f"
-            set fileDeleted=true
-        ) else (
-            echo File not found: %%f
-        )
-    )
-    
-    rem Confirmation dialog if the file was deleted
-    if "%fileDeleted%" == "true" (
-        powershell -command "Add-Type -AssemblyName PresentationCore,PresentationFramework;[System.Windows.MessageBox]::Show('File(s) deleted successfully.', 'Confirmation')"
+  rem Navigate to the CrowdStrike directory
+  cd /d %WINDIR%\System32\drivers\CrowdStrike
+  
+  rem Delete the matching file
+  set fileDeleted=false
+  for %%f in (C-00000291*.sys) do (
+    if exist "%%f" (
+      del /f /q "%%f"
+      set fileDeleted=true
     ) else (
-        powershell -command "Add-Type -AssemblyName PresentationCore,PresentationFramework;[System.Windows.MessageBox]::Show('No matching file found to delete.', 'Confirmation')"
+      echo File not found: %%f
     )
-
-    rem Boot into normal mode
+  )
+  
+  rem Confirmation message with choice
+  if "%fileDeleted%" == "true" (
+    echo File(s) deleted successfully.
+  ) else (
+    echo No matching file found to delete.
+  )
+  
+  choice /c YN /m Do you want to reboot to normal mode? && (
     bcdedit /deletevalue {current} safeboot
-
-    rem Restart the system again
     shutdown /r /t 0
+  )
 )
 
 endlocal
