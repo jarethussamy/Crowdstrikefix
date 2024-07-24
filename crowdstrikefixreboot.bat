9b100e02452b4e23c3e04ee2c9da304cec0cdc9b
jarethussamy <42392057+jarethussamy@users.noreply.github.com>
2024-07-24 18:04:02 -0400

    Update crowdstrikefixreboot.bat

--- crowdstrikefixreboot.bat
+++ crowdstrikefixreboot.bat
@@ -1,4 +1,3 @@
-
 @echo off
 setlocal
 
@@ -17,42 +16,36 @@ rem Restart the system
 shutdown /r /t 0
 timeout /t 60 /nobreak
 
-rem After reboot, we should be in Safe Mode
+rem Ensure that we are now in Safe Mode
 :SafeMode
-netsh wlan show interfaces >nul 2>&1
-if %errorLevel% neq 0 (
-    echo Not in Safe Mode with Networking, exiting...
-    pause
-    exit /b 1
-)
-
-rem Navigate to the CrowdStrike directory
-cd /d %WINDIR%\System32\drivers\CrowdStrike
-
-rem Delete the matching file
-set fileDeleted=false
-for %%f in (C-00000291*.sys) do (
-    if exist "%%f" (
-        del /f /q "%%f"
-        set fileDeleted=true
-    ) else (
-        echo File not found: %%f
+rem Check if we are in Safe Mode with Networking
+if /i "%SAFEBOOT_OPTION%"=="network" (
+    rem Navigate to the CrowdStrike directory
+    cd /d %WINDIR%\System32\drivers\CrowdStrike
+    
+    rem Delete the matching file
+    set fileDeleted=false
+    for %%f in (C-00000291*.sys) do (
+        if exist "%%f" (
+            del /f /q "%%f"
+            set fileDeleted=true
+        ) else (
+            echo File not found: %%f
+        )
     )
+    
+    rem Confirmation dialog if the file was deleted
+    if "%fileDeleted%" == "true" (
+        powershell -command "Add-Type -AssemblyName PresentationCore,PresentationFramework;[System.Windows.MessageBox]::Show('File(s) deleted successfully.', 'Confirmation')"
+    ) else (
+        powershell -command "Add-Type -AssemblyName PresentationCore,PresentationFramework;[System.Windows.MessageBox]::Show('No matching file found to delete.', 'Confirmation')"
+    )
+
+    rem Boot into normal mode
+    bcdedit /deletevalue {current} safeboot
+
+    rem Restart the system again
+    shutdown /r /t 0
 )
-
-rem Confirmation dialog if the file was deleted
-if "%fileDeleted%" == "true" (
-    echo File(s) deleted successfully.
-    pause
-) else (
-    echo No matching file found to delete.
-    pause
-)
-
-rem Boot back into normal mode
-bcdedit /deletevalue {current} safeboot
-
-rem Restart the system again
-shutdown /r /t 0
 
 endlocal
