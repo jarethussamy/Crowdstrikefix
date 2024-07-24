e67618bfa1e156a6b60a2b17db306c7b353b43b0
jarethussamy <42392057+jarethussamy@users.noreply.github.com>
2024-07-24 18:21:29 -0400

    Update crowdstrikefixreboot.bat

--- crowdstrikefixreboot.bat
+++ crowdstrikefixreboot.bat
@@ -1,12 +1,12 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal
 
 rem Check if running as administrator
 net session >nul 2>&1
 if %errorLevel% neq 0 (
     echo This script must be run as an administrator.
-    powershell -Command "Start-Process -FilePath '%0' -Verb RunAs"
-    exit /b
+    pause
+    exit /b 1
 )
 
 rem Boot into Safe Mode with Networking
@@ -19,26 +19,26 @@ timeout /t 60 /nobreak
 rem Ensure that we are now in Safe Mode
 :SafeMode
 rem Check if we are in Safe Mode with Networking
-if /i "%SAFEBOOT_OPTION%"=="NETWORK" (
+if /i "%SAFEBOOT_OPTION%"=="network" (
     rem Navigate to the CrowdStrike directory
-    cd /d "%WINDIR%\System32\drivers\CrowdStrike"
+    cd /d %WINDIR%\System32\drivers\CrowdStrike
     
     rem Delete the matching file
-    set "fileDeleted=false"
+    set fileDeleted=false
     for %%f in (C-00000291*.sys) do (
         if exist "%%f" (
             del /f /q "%%f"
-            set "fileDeleted=true"
+            set fileDeleted=true
         ) else (
             echo File not found: %%f
         )
     )
     
     rem Confirmation dialog if the file was deleted
-    if "!fileDeleted!" == "true" (
-        powershell -Command "Add-Type -AssemblyName PresentationCore,PresentationFramework;[System.Windows.MessageBox]::Show('File(s) deleted successfully.', 'Confirmation')"
+    if "%fileDeleted%" == "true" (
+        powershell -command "Add-Type -AssemblyName PresentationCore,PresentationFramework;[System.Windows.MessageBox]::Show('File(s) deleted successfully.', 'Confirmation')"
     ) else (
-        powershell -Command "Add-Type -AssemblyName PresentationCore,PresentationFramework;[System.Windows.MessageBox]::Show('No matching file found to delete.', 'Confirmation')"
+        powershell -command "Add-Type -AssemblyName PresentationCore,PresentationFramework;[System.Windows.MessageBox]::Show('No matching file found to delete.', 'Confirmation')"
     )
 
     rem Boot into normal mode
@@ -46,9 +46,6 @@ if /i "%SAFEBOOT_OPTION%"=="NETWORK" (
 
     rem Restart the system again
     shutdown /r /t 0
-) else (
-    echo Not in Safe Mode with Networking. Exiting...
-    exit /b 1
 )
 
 endlocal
